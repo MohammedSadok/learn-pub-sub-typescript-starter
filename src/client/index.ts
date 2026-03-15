7287c9ad1b328d6c7599c7da7e1bba55e89c945b
Mohammed Sadok <mohammed.sadok.2003@gmail.com>
2026-03-15 11:44:24 +0000

    add declareAndBind function and update main to handle RabbitMQ queue binding

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,43 @@
+import amqp from "amqplib";
+import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import {
+  declareAndBind,
+  SimpleQueueType,
+} from "../internal/pubsub/declareAndBind.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+
 async function main() {
   console.log("Starting Peril client...");
+
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+
+  const username = await clientWelcome();
+
+  const queueName = `pause.${username}`;
+  const [ch, queue] = await declareAndBind(
+    conn,
+    ExchangePerilDirect,
+    queueName,
+    PauseKey,
+    SimpleQueueType.Transient,
+  );
+
+  console.log(`Queue "${queue.queue}" created and bound!`);
+
+  ["SIGINT", "SIGTERM"].forEach((signal) =>
+    process.on(signal, async () => {
+      try {
+        await ch.close();
+        await conn.close();
+        console.log("RabbitMQ connection closed.");
+      } catch (err) {
+        console.error("Error closing RabbitMQ connection:", err);
+      } finally {
+        process.exit(0);
+      }
+    }),
+  );
 }
 
 main().catch((err) => {
