f2947dda0d4d0cabf406c4a0d775b0e0395c8517
Mohammed Sadok <mohammed.sadok.2003@gmail.com>
2026-03-15 11:15:45 +0000

    implement publishJSON function and update main to publish pause message

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,22 @@
 import amqp from "amqplib";
+import type { PlayingState } from "../internal/gamelogic/gamestate.js";
+import { publishJSON } from "../internal/pubsub/publishjson.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
 
 async function main() {
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
   const conn = await amqp.connect(rabbitConnString);
+  const ch = await conn.createConfirmChannel();
   console.log("Peril game server connected to RabbitMQ!");
+
+  const pausedState: PlayingState = { isPaused: true };
+  await publishJSON(ch, ExchangePerilDirect, PauseKey, pausedState);
+  console.log("Published pause message to peril_direct exchange.");
 
   ["SIGINT", "SIGTERM"].forEach((signal) =>
     process.on(signal, async () => {
       try {
+        await ch.close();
         await conn.close();
         console.log("RabbitMQ connection closed.");
       } catch (err) {
